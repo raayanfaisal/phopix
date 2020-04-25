dcb3a7f2f9ac9e32159c11395ed0c479dbf88270
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-04-25 07:48:14 -0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,7 @@
         </div>
       </li>
     </ul>
-    <a href="" class="ml-3"><img src="/images/06-facebook-512.png" alt="" width="20px" height="20px"></a>
+    <a href="" class="ml-3"><img src="https://raw.githubusercontent.com/raayanfaisal/phopix/master/public/Images/06-facebook-512.png" alt="" width="20px" height="20px"></a>
     <a href="" class="ml-3"><img src="/images/twitter.png" alt="" width="20px" height="20px"></a>
     <a href="" class="ml-3"><img src="/images/insta.png" alt="" width="30px" height="30px"></a>
     
