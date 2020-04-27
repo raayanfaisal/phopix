b5555d713308a319254ab8741b60994d913c0baa
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-04-27 01:50:54 -0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -84,7 +84,7 @@
       <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
 
         <!-- Links -->
-        <h5 class="font-weight-bold text-uppercase mb-4">About</h5>
+        <h5 class="font-weight-bold text-uppercase mb-4">Usefull Links</h5>
 
         <ul class="list-unstyled">
           <li>
