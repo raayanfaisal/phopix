4c4514412d77a8f95403dd9c7a75e68ce1246a4a
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-04-28 14:45:39 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.index');
+    return view('index');
 });
-
-Route::post('/', 'ContactController@index');
 
 Auth::routes();
 
-
+Route::get('/index', 'ContactController@index');
 
 Route::get('/home', 'HomeController@index')->name('home');
