558fa563e67dd9850e6917e5fd2f5fad2ba0381f
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-04-28 13:07:12 -0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
             Phopix is a web development team based in the republic of Maldives . Currently we are a team of 5 young web developers , <b>Note Site under development</b>
             </h4>
             <a href="#cont" class="btn btn-primary btn-lg active m-5" role="button" aria-pressed="true">Hire Us!</a>
-            <img src="/images/demo.png" alt="Recent work" width="100%" height="auto">
+            <img src="https://raw.githubusercontent.com/raayanfaisal/phopix/master/public/Images/demo.png" alt="Recent work" width="100%" height="auto">
         </div>
     </div>
 <!-- ======= End Hero Section ======= -->
