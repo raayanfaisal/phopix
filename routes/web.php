59b4bd87f50881787624bb49ea59dc5342365c19
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-04-29 00:46:32 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/index', 'ContactController@index');
+Route::post('/index', 'ContactController@index');
 
 Route::get('/home', 'HomeController@index')->name('home');
