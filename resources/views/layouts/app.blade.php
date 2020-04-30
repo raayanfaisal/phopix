7435b7bb29d56349c513e6c1efce37c2a632c8b3
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-04-30 05:00:29 -0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -153,7 +153,7 @@
     <div id="app">
         <header>
             <div class="container mt-5">
-            <nav class="navbar navbar-expand-lg navbar-light bg-white" style="">
+            <nav class="navbar navbar-expand-lg navbar-light bg-#F2F5F7" style="">
   <a class="navbar-brand" href="#"><img src="https://raw.githubusercontent.com/raayanfaisal/phopix/master/public/Images/hlogo.png" alt="logo" width="40px" height="40px"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
