df1670be4461a37140518f80df888aa1267b8468
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-04-30 05:05:51 -0700

    Add files via upload

---
+++ resources/views/subscriptions.blade.php
@@ -0,0 +1,113 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="card-group mt-5">
+    <div class="card" style="width: 18rem;" id="cd">
+  <img class="card-img-top" src="/images/basic.png" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">$50</h2>
+    <h2 class="card-title">$500</h2>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item" style="background-color: #F2F5F7;">Upto 2 pages</li>
+    <li class="list-group-item" style="background-color: #F2F5F7;">Commercial usage</li>
+    <li class="list-group-item" style="background-color: #F2F5F7;">5 revisions</li>
+    <li class="list-group-item" style="background-color: #F2F5F7;">No maintanance</li>
+    <li class="list-group-item" style="background-color: #F2F5F7;">Custom Logo</li>
+    <li class="list-group-item" style="background-color: #F2F5F7;">Responsive Layout</li>
+  </ul>
+  <div class="card-body">
+  <a href="https://forms.gle/5Ro8mnCCLcEfdZnq5" class="btn" id="button">Choose plan</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;" id="cd">
+  <img class="card-img-top" src="/images/plus.png" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">$90</h2>
+    <h2 class="card-title">$1000</h2>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush" style=" ">
+    <li class="list-group-item" style="background-color: #F2F5F7;">upto 5 pages</li>
+    <li class="list-group-item" style="background-color: #F2F5F7; ">Commecial and personal usage</li>
+    <li class="list-group-item" style=" background-color: #F2F5F7;">Upto 10 revisions</li>
+    <li class="list-group-item" style=" background-color: #F2F5F7;">2 month maintanance</li>
+    <li class="list-group-item" style=" background-color: #F2F5F7;">Design customization</li>
+    <li class="list-group-item" style="background-color: #F2F5F7; ">Website updates</li>
+    <li class="list-group-item" style="background-color: #F2F5F7; ">responsive designs</li>
+  </ul>
+  <div class="card-body">
+    <a href="https://forms.gle/5Ro8mnCCLcEfdZnq5" class="btn " id="button">Choose plan</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;" id="cd">
+  <img class="card-img-top" src="/images/premium.png" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">$150</h2>
+    <h2 class="card-title">$1700</h2>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item" style="background-color: #F2F5F7;">upto 15 pages</li>
+    <li class="list-group-item" style="background-color: #F2F5F7;">Commercial and Personal Licence</li>
+    <li class="list-group-item" style="background-color: #F2F5F7;">Fully resposive</li>
+    <li class="list-group-item" style="background-color: #F2F5F7;">E-Commerce Functions</li>
+    <li class="list-group-item" style="background-color: #F2F5F7;">Content Uploads</li>
+    <li class="list-group-item" style="background-color: #F2F5F7;">Website updates</li>
+    <li class="list-group-item" style="background-color: #F2F5F7;">5 months maintanance</li>
+    <li class="list-group-item" style="background-color: #F2F5F7;">15 revisions</li>
+  </ul>
+  <div class="card-body">
+    <a href="https://forms.gle/5Ro8mnCCLcEfdZnq5" class="btn" id="button">Choose plan</a>
+  </div>
+</div>
+    </div>
+</div>
+<div class="container-fluid mt-5">
+<!-- Footer -->
+<footer class="page-footer font-small special-color-dark pt-4" style=" "> 
+
+  <!-- Footer Elements -->
+  <div class="container">
+    <div class="text-center mb-2">
+          <a href="" class="text-white">Privacy Policy |</a>
+
+          <a href="" class="text-white">Terms and Condition's |</a>
+
+          <a href="https://forms.gle/5Ro8mnCCLcEfdZnq5" class="text-white">Back to Top </a>
+      </div>
+    <!-- Social buttons -->
+    <ul class="list-unstyled list-inline text-center">
+      <li class="list-inline-item">
+        <a class="btn-floating btn-fb mx-1">
+          <i class="fab fa-facebook-f"><a href=""><img src="/images/facebook.png" alt="" width="50px" height="50px"></a></i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-tw mx-1">
+          <i class="fab fa-twitter"><a href="https://www.instagram.com/phopix.mv/"><img src="/images/inst.png" alt="" width="50px" height="50px"></a></i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-gplus mx-1">
+          <i class="fab fa-google-plus-g"><a href="https://twitter.com/PhopixM"><img src="/images/twtt.png" alt="" width="50px" height="50px"></a></i>
+        </a>
+      </li>
+    </ul>
+    <!-- Social buttons -->
+    
+  </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="" style="" class="text-white"> Phopix.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+</div>
+@endsection
