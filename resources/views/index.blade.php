056b0ac22d434a07d0420535c899dde2e16b2c07
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-01 13:13:51 -0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,9 @@
 <div class="container pt-5 mt-5" style="background-color: #F2F5F7">
     <div class="row">
         <div class="col-12">
-            <h1>
+            <h1><b>
                 Welcome to PhoPix.
-            </h1>
+            </h1></b>
             <h4 class="pt-4">
             Phopix is a web development team based in the republic of Maldives . Currently we are a team of 5 young web developers , <b>Note Site under development</b>
             </h4>
@@ -18,16 +18,16 @@
 <!-- ======= About Section ======= -->
     <div class="row">
         <div class="col-12 mt-5 pt-5" id="abt">
-            <h2>
+            <h2><b>
                 Who are we?
-            </h2>
+            </h2></b>
             <p>
             Phopix private limited is a company based in the republic of Maldives. And we offer a variety of services, and we strive to do them as near to perfection as we can get . To ensure maximum customer satisfaction in the end . We specialize in HTML5, CSS3, Laravel, MySQL and WordPress for building custom high-end websites that scale well on all your devices
             </p>
 <!-- ======= Service Section ======= -->
-            <h2 class="mt-4">
+            <h2 class="mt-4"><b>
                 Services we offer.
-            </h2>
+            </h2></b>
             <p>
               Below are our services
             </p>
@@ -35,18 +35,18 @@
                 <div class="card-group">
                     <div class="card m-2" style="" id="cd">
                         <div class="card-body">
-                            <h4 class="card-title">
+                            <h4 class="card-title"><b>
                               Web design and development.
-                            </h4>
+                            </h4></b>
                             <p>we will design and build for you a high performance  website</p>
                         </div>
                     </div>
 
                     <div class="card m-2" style="" id="cd">
                         <div class="card-body">
-                            <h4 class="card-title">
+                            <h4 class="card-title"><b>
                               Website Maintanance.
-                            </h4>
+                            </h4></b>
                             <p>We will maintain your existing website and keep it in running condition 24/7</p>
                         </div>
                     </div>
@@ -55,18 +55,18 @@
                 <div class="card-group">
                     <div class="card m-2" style="" id="cd">
                         <div class="card-body">
-                            <h4 class="card-title">
+                            <h4 class="card-title"><b>
                               Application development. 
-                            </h4>
+                            </h4></b>
                             <p>we will build apps that can run on both IOS and Android</p>
                         </div>
                     </div>
 
                     <div class="card m-2" style="" id="cd">
                         <div class="card-body">
-                            <h4 class="card-title">
+                            <h4 class="card-title"><b>
                               Social media page management.
-                            </h4>
+                            </h4></b>
                             <p>any platform with a affordable price</p>
                         </div>
                     </div>
@@ -75,18 +75,18 @@
                 <div class="card-group">
                     <div class="card m-2" style="" id="cd">
                         <div class="card-body">
-                            <h4 class="card-title">
+                            <h4 class="card-title"><b>
                               Logo Designing 
-                            </h4>
+                            </h4></b>
                             <p>Custom logos that comply with modern trends and is upto your specs</p>
                         </div>
                     </div>
 
                     <div class="card m-2" style="" id="cd">
                         <div class="card-body">
-                            <h4 class="card-title">
+                            <h4 class="card-title"><b>
                               Website management
-                            </h4>
+                            </h4></b>
                             <p>We will manage your website, uploading picture and making corrections</p>
                         </div>
                     </div>
