7402a9d32aa709ccdbc22b0a6341eeba4d825f8e
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-01 11:17:43 -0700

    Update subscriptions.blade.php

--- resources/views/subscriptions.blade.php
+++ resources/views/subscriptions.blade.php
@@ -68,7 +68,7 @@
 </div>
     </div>
 </div>
-<div class="container-fluid">
+<div class="container-fluid mt-5">
 <!-- Footer -->
 <footer class="page-footer font-small special-color-dark pt-4" style="" id="ft"> 
 
