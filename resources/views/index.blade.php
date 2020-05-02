29c085b5c6dfb90c48a2a4d79c1b52eeeff5483f
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-02 16:35:57 -0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -129,50 +129,7 @@
 </div>
 <div class="container-fluid">
 <!-- Footer -->
-<footer class="page-footer font-small special-color-dark pt-4" style="" id="ft"> 
 
-  <!-- Footer Elements -->
-  <div class="container">
-    <div class="text-center mb-2">
-          <a href="/privacy" class="text-info">Privacy Policy |</a>
-
-          <a href="/terms" class="text-info">Terms and Condition's |</a>
-
-          <a href="#" class="text-info">Back to Top </a>
-      </div>
-    <!-- Social buttons -->
-    <ul class="list-unstyled list-inline text-center">
-      <li class="list-inline-item">
-        <a class="btn-floating btn-fb mx-1">
-          <a href="https://www.facebook.com/Phopix-108547830842311/?modal=admin_todo_tour&notif_id=1588117275340198&notif_t=page_invite&ref=notif"><i class="fab fa-facebook-f" id="ci"></i></a>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-tw mx-1">
-          <a href="https://twitter.com/PhopixM"><i class="fab fa-twitter" id="ci"></i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-gplus mx-1">
-          <a href="https://www.instagram.com/phopix.mv/"><i class="fab fa-instagram" id="ci"></i></a>
-        </a>
-      </li>
-    </ul>
-    <!-- Social buttons -->
-    
-  </div>
-  <!-- Footer Elements -->
-
-  <!-- Copyright -->
-  <div class="footer-copyright text-center">7815551 / 9682212
-  </div>
-  <div class="footer-copyright text-center py-3">© 2020 Copyright:
-    <a href="" style="" class="text-info"> phopix.com</a>
-  </div>
-  <!-- Copyright -->
-
-</footer>
-<!-- Footer -->
 </div>
 <!-- ======= Service Section ======= -->
 <!-- ======= End About Section ======= -->
