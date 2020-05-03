ec9facd7069d31471399f17187889f6e26bc3917
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-03 14:21:44 -0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="./dist/css/light/neumorph-full.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://github.com/raayanfaisal/phopix/blob/master/public/css/style.css" rel="stylesheet">
    
 </head>
 <body>
