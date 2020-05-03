9a6d40cc420fcc3bf6ad900961d03a04d0d46d66
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-03 14:12:50 -0700

    Update subscriptions.blade.php

--- resources/views/subscriptions.blade.php
+++ resources/views/subscriptions.blade.php
@@ -1,77 +1,88 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="card-group mt-5">
-    <div class="card" style="width: 18rem;" id="cd">
- 
-  <div class="card-body">
-    <h2 class="card-title">Basic Plan</h2>
-    <h2 class="card-title">$50<small>/month</small></h2>
-    <h2 class="card-title">$500<small>/year</small></h2>
-    
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item" style="background-color: #F2F5F7;">Upto 2 pages</li>
-    <li class="list-group-item" style="background-color: #F2F5F7;">Commercial usage</li>
-    <li class="list-group-item" style="background-color: #F2F5F7;">5 revisions</li>
-    <li class="list-group-item" style="background-color: #F2F5F7;">No maintanance</li>
-    <li class="list-group-item" style="background-color: #F2F5F7;">Custom Logo</li>
-    <li class="list-group-item" style="background-color: #F2F5F7;">Responsive Layout</li>
-  </ul>
-  <div class="card-body">
-  <a href="https://forms.gle/QoqUikZYCKy9PrKd9" class="btn" id="button">Choose plan</a>
-  </div>
-</div>
-<div class="card" style="width: 18rem;" id="cd">
-  
-  <div class="card-body">
-    <h2 class="card-title">Plus Plan</h2>
-    <h2 class="card-title">$90<small>/month</small></h2>
-    <h2 class="card-title">$1000<small>/year</small></h2>
-    
-  </div>
-  <ul class="list-group list-group-flush" style=" ">
-    <li class="list-group-item" style="background-color: #F2F5F7;">upto 5 pages</li>
-    <li class="list-group-item" style="background-color: #F2F5F7; ">Commecial and personal usage</li>
-    <li class="list-group-item" style=" background-color: #F2F5F7;">Upto 10 revisions</li>
-    <li class="list-group-item" style=" background-color: #F2F5F7;">2 month maintanance</li>
-    <li class="list-group-item" style=" background-color: #F2F5F7;">Design customization</li>
-    <li class="list-group-item" style="background-color: #F2F5F7; ">Website updates</li>
-    <li class="list-group-item" style="background-color: #F2F5F7; ">responsive designs</li>
-  </ul>
-  <div class="card-body">
-    <a href="https://forms.gle/QoqUikZYCKy9PrKd9" class="btn " id="button">Choose plan</a>
-  </div>
-</div>
-<div class="card" style="width: 18rem;" id="cd">
- 
-  <div class="card-body">
-    <h2 class="card-title">Premium Plan</h2>
-    <h2 class="card-title">$150<small>/month</small></h2>
-    <h2 class="card-title">$1700<small>/year</small></h2>
-    
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item" style="background-color: #F2F5F7;">upto 15 pages</li>
-    <li class="list-group-item" style="background-color: #F2F5F7;">Commercial and Personal Licence</li>
-    <li class="list-group-item" style="background-color: #F2F5F7;">Fully resposive</li>
-    <li class="list-group-item" style="background-color: #F2F5F7;">E-Commerce Functions</li>
-    <li class="list-group-item" style="background-color: #F2F5F7;">Content Uploads</li>
-    <li class="list-group-item" style="background-color: #F2F5F7;">Website updates</li>
-    <li class="list-group-item" style="background-color: #F2F5F7;">5 months maintanance</li>
-    <li class="list-group-item" style="background-color: #F2F5F7;">15 revisions</li>
-  </ul>
-  <div class="card-body">
-    <a href="https://forms.gle/QoqUikZYCKy9PrKd9" class="btn" id="button">Choose plan</a>
-  </div>
-</div>
+<section>
+  <div class="container">
+      <h3 class="text-center mt-5 mb-4">Choose the plan that is more suitable to you!</h3>
+      <table class="table mt-5">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Plan features</th>
+          <th scope="col">Basic Package</th>
+          <th scope="col">Plus Package</th>
+          <th scope="col">Premium package</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>No of page's</td>
+          <td>2</td>
+          <td>5</td>
+          <td>15</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Commercial Usage</td>
+          <td>yes</td>
+          <td>yes</td>
+          <td>yes</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>No of revisions</td>
+          <td>5</td>
+          <td>10</td>
+          <td>15</td>
+        </tr>
+        <tr>
+          <th scope="row">4</th>
+          <td>Web site maintanance</td>
+          <td>No</td>
+          <td>2 months</td>
+          <td>5 months</td>
+        </tr>
+        <tr>
+          <th scope="row">5</th>
+          <td>Custom logo</td>
+          <td>No</td>
+          <td>Yes</td>
+          <td>Yes</td>
+        </tr>
+        <tr>
+          <th scope="row">6</th>
+          <td>Responsive layout</td>
+          <td>Yes</td>
+          <td>Yes</td>
+          <td>Yes</td>
+        </tr>
+        <tr>
+          <th scope="row">6</th>
+          <td>design customization</td>
+          <td>No</td>
+          <td>Yes</td>
+          <td>Yes</td>
+        </tr>
+        <tr>
+          <th scope="row">6</th>
+          <td>Website Updates</td>
+          <td>No</td>
+          <td>Yes</td>
+          <td>Yes</td>
+        </tr>
+        <tr>
+          <th scope="row">6</th>
+          <td>E-Commerce Functionality</td>
+          <td>No</td>
+          <td>No</td>
+          <td>Yes</td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="text-center">
+      <a href="" class="btn btn-primary btn-lg">Continue</a>
     </div>
-</div>
-<div class="container-fluid mt-5">
-<!-- Footer -->
-
-</div>
-<!-- Footer -->
-</div>
+  </div>
+<section>
 @endsection
