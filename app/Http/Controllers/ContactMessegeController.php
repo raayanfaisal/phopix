082ef648d7e6d0e53726d0525f0d9a810b221edc
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-13 05:35:11 -0700

    Add files via upload

---
+++ app/Http/Controllers/ContactMessegeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Mail;
+use Illuminate\Http\Request;
+
+
+class ContactMessegeController extends Controller
+{
+    public function create()
+    {
+        return view('pages.contact');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'details' => 'required'
+        ]);
+
+        Mail::send('emails.messege', [
+            'dts' => $request->details
+        ], function ($mail) use($request) {
+
+              $mail->from($request->email, $request->name);
+              
+              $mail->to('info.phopix@gmail.com')->subject('Contact Messege');
+        });
+
+        return redirect()->back()->with('flash_messege', 'Thank you for your messege.');
+    }
+}
