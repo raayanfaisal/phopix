4eed6ae6df3ffc62aea7d17591af389bbd614460
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-13 05:46:01 -0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@ h1 {
         <header style="background-color: #FFFF;">
             <div class="container mt-5">
             <nav class="navbar navbar-expand-lg navbar-light bg-#FFFF" style="background-color: #FFFF;">
-  <a class="navbar-brand" href="#"><img src="https://raw.githubusercontent.com/raayanfaisal/phopix/master/public/Images/hlogo.png" alt="logo" width="40px" height="40px"></a>
+  <a class="navbar-brand" href="#"><img src="https://raw.githubusercontent.com/raayanfaisal/phopix/master/public/Images/phopix.svg" alt="logo" width="100px" height="100px"></a>
   <a class="navbar-brand" href="#">PhoPix</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
