479981d498a7f8ddbd402871115504a66798bec7
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-20 19:20:51 -0700

    Add files via upload

---
+++ resources/views/w2.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h2 class="text-white" id="w4">Morning Leaque</h2>
+            <p class="text-white">Logo designed and commisioned by PhoPix for Morning Leaque a football leaque by stelco.</p>
+            <p class="text-white">Completed march 14 2019</p>
+            <a href="/portfolio" class="btn btn-primary btn-lg">Go back</a>
+        </div>
+
+        <div class="col-md-6 mt-5 mb-5">
+            <img src="https://raw.githubusercontent.com/raayanfaisal/phopix/09d902f0b40e028a96bec8b152cf85b5d3b16615/public/Images/Clogo.png" alt="work1" width="100%" height="100%">
+        </div>
+    </div>
+</div>
+@endsection
