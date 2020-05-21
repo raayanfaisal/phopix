20e2aafa088351da5b7a797ee418f18e914fd9d5
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-20 19:25:12 -0700

    Update w2.blade.php

--- resources/views/w2.blade.php
+++ resources/views/w2.blade.php
@@ -7,7 +7,7 @@
             <h2 class="text-white" id="w4">Morning Leaque</h2>
             <p class="text-white">Logo designed and commisioned by PhoPix for Morning Leaque a football leaque by stelco.</p>
             <p class="text-white">Completed march 14 2019</p>
-            <a href="/portfolio" class="btn btn-primary btn-lg">Go back</a>
+            <a href="/work" class="btn btn-primary btn-lg">Go back</a>
         </div>
 
         <div class="col-md-6 mt-5 mb-5">
