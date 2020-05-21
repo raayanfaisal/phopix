0d4ffc34aeca33d963a12941392f80838a76ce3d
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-20 17:24:18 -0700

    Add files via upload

---
+++ resources/views/w1.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h2 id="w3">Website ( On-Going )</h2>
+            <p class="text-white">On going project a portfolio website for a local business</p>
+        </div>
+
+        <div class="col-md-6 mt-5 mb-5">
+            <img src="/images/w3.png" alt="work1" width="100%" height="100%">
+        </div>
+    </div>
+</div>
+@endsection
