1443285ff74a596acf42acb7820215e92c1ea7eb
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-20 19:27:39 -0700

    Add files via upload

---
+++ resources/views/w3.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h2 id="w3">Wraps</h2>
+            <p class="text-white">Logo designed by PhoPix for Wraps.</p>
+            <a href="/work" class="btn btn-primary btn-lg">Go back</a>
+        </div>
+
+        <div class="col-md-6 mt-5 mb-5">
+            <img src="https://raw.githubusercontent.com/raayanfaisal/phopix/09d902f0b40e028a96bec8b152cf85b5d3b16615/public/Images/w3.png" alt="work1" width="100%" height="100%">
+        </div>
+    </div>
+</div>
+@endsection
