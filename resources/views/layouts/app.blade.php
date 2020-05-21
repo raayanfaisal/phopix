1c90fbf7f836f196fb67ea866c9eae58e0bceda8
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-21 13:43:57 -0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="./dist/css/light/neumorph-full.min.css" />
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
     <style>
       html {
         scroll-behavior: smooth;
