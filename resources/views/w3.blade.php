152f987c3b23bcb74eef0979c9c2519dc939814c
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-20 19:29:42 -0700

    Update w3.blade.php

--- resources/views/w3.blade.php
+++ resources/views/w3.blade.php
@@ -10,7 +10,7 @@
         </div>
 
         <div class="col-md-6 mt-5 mb-5">
-            <img src="https://raw.githubusercontent.com/raayanfaisal/phopix/09d902f0b40e028a96bec8b152cf85b5d3b16615/public/Images/w3.png" alt="work1" width="100%" height="100%">
+            <img src="https://raw.githubusercontent.com/raayanfaisal/phopix/09d902f0b40e028a96bec8b152cf85b5d3b16615/public/Images/C2logo.png" alt="work1" width="100%" height="100%">
         </div>
     </div>
 </div>
