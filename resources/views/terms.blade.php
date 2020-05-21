c8987165579ed0b2fea144691e6a84e202e1c3e9
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-21 14:07:02 -0700

    Update terms.blade.php

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -2,54 +2,54 @@
 
 @section('content')
 <div class="container mt-5 text-white">
-<h1>
+<h1 class="text-white">
     Terms and Condition
 </h1>
-<h2>
+<h2 class="text-white">
     1 . terms
 </h2>
-<p>
+<p class="text-white">
 By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of Use, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this web site are protected by applicable copyright and trade mark law.
 </p>
-<h2>
+<h2 class="text-white">
     2 . User interface
 </h2>
-<p>
+<p class="text-white">
 a. Permission is granted to temporarily download one copy of the materials (information or software) on Company's web site for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
 </p>
-<ul>
-    <li>Modify or copy the materials</li>
-    <li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial)</li>
-    <li>attempt to decompile or reverse engineer any software contained on Company's web site</li>
-    <li>iv.	remove any copyright or other proprietary notations from the materials; or
+<ul class="text-white">
+    <li class="text-white">Modify or copy the materials</li>
+    <li class="text-white">use the materials for any commercial purpose, or for any public display (commercial or non-commercial)</li>
+    <li class="text-white">attempt to decompile or reverse engineer any software contained on Company's web site</li>
+    <li class="text-white">iv.	remove any copyright or other proprietary notations from the materials; or
         v.	transfer the materials to another person or 'mirror' the materials on any other server.
     </li>
 </ul>
-<p>
+<p class="text-white">
 b.	This license shall automatically terminate if you violate any of these restrictions and may be terminated by Company at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format
 </p>
-<h2>
+<h2 class="text-white">
     Disclaimer
 </h2>
-<p>
+<p class="text-white">
     The materials on Company's web site are provided 'as is'. Company makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, Company does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet web site or otherwise relating to such materials or on any sites linked to this site.
 </p>
-<h2>
+<h2 class="text-white">
     limitation
 </h2>
-<p>
+<p class="text-white">
 In no event shall Company or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on Company's Internet site, even if Company or a Company authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
 </p>
-<h2>
+<h2 class="text-white">
     Revision and Errata
 </h2>
-<p>
+<p class="text-white">
 The materials appearing on Company's web site could include technical, typographical, or photographic errors. Company does not warrant that any of the materials on its web site are accurate, complete, or current. Company may make changes to the materials contained on its web site at any time without notice. Company does not, however, make any commitment to update the materials. 
 </p>
-<h2>
+<h2 class="text-white">
     Site terms of use modification
 </h2>
-<p>
+<p class="text-white">
 Company may revise these terms of use for its web site at any time without notice. By using this web site, you are agreeing to be bound by the then current version of these Terms and Conditions of Use.
 
 8.	Governing Law
