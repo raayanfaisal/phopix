173987376a7810a7e0d9b16540a6e99c35224a6d
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-05-23 12:17:47 -0700

    Update nav.js

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,7 +1,41 @@
-function closeMenu(){
-  document.getElementById("navbar").style.height = "0px";
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function(){
+  if(this.checked){
+      trans()
+      document.documentElement.setAttribute('data-theme', 'dark-mode')
+  } else {
+    trans()
+    document.documentElement.setAttribute('data-theme', 'light')
+  }
+})
+
+let trans = () => {
+  document.documentElement.classList.add('transition');
+  window.setTimeout(() => {
+      document.documentElement.classList.remove('transition')
+  }, 1000)
 }
 
-function openMenu(){
-  document.getElementById("navbar").style.height = "1000px";
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
 }
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+AOS.init();
+
+$(function() {
+  var selectedClass = "";
+  $(".filter").click(function(){
+  selectedClass = $(this).attr("data-rel");
+  $("#gallery").fadeTo(100, 0.1);
+  $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+  setTimeout(function() {
+  $("."+selectedClass).fadeIn().addClass('animation');
+  $("#gallery").fadeTo(300, 1);
+  }, 300);
+  });
+  });
