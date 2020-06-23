fe44d47180262742c437b658c6eed27ca61b4c6e
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-06-23 12:07:32 -0700

    Update subscriptions.blade.php

--- resources/views/subscriptions.blade.php
+++ resources/views/subscriptions.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-   <h1 class="text-center">PAGE UNDER MAINTANANCE</h1>
-   <p class="text-center">For inquiries send us a email at phopixofficial@gmail.com or contact 9682212 or 7815551</p>
+   <h1 class="text-center" style="color: white;">PAGE UNDER MAINTANANCE</h1>
+   <p class="text-center" style="color: white;">For inquiries send us a email at phopixofficial@gmail.com or contact 9682212 or 7815551</p>
 </div>
 @endsection
